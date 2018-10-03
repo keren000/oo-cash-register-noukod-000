948cf66471a7120592ca3dcb0e857d5b696e35dc
Keren-Happuc Fleurimond <keren@noukod.com>
2018-10-03 19:16:56 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,29 @@
+class Cash_Register
+  attr_accessor :items, :discount, :total, :last_transaction
 
+  def initialize(discount = 0)
+    @discount = discount
+    @total = 0
+    @items = []
+  end
+
+  def add_item(item_title, cost, quantity = 1)
+    self.total += cost * quantity
+    quantity.times do
+      items << item_title
+    end
+    self.last_transaction = cost * quantity
+  end
+
+  def apply_discount
+    if discount == 0
+      return "There is no discount to apply"
+    end
+    self.total -= total * discount / 100
+    return "After the discount, the total comes to $#{self.total}."
+    end
+
+    def void_last_transaction
+      self.total -= self.last_transaction
+    end 
+end
