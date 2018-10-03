b24567ec047bc5abd0fe358dce13c5adbe6e045b
Keren-Happuc Fleurimond <keren@noukod.com>
2018-10-03 19:25:10 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,12 +16,18 @@ class CashRegister
   end
 
   def apply_discount
-    if discount == 0
-      return "There is no discount to apply"
+    # if discount == 0
+    #   return "There is no discount to apply"
+    # else
+    # self.total -= total * discount / 100
+    # return "After the discount, the total comes to $#{self.total}."
+    # end
+    if discount!= 0
+      self_total =m(total *((100.0 - discount.to_f)/100)).to_i
+      return "After the discount, the total comes to $#{self.total}."
     else
-    self.total -= total * discount / 100
-    return "After the discount, the total comes to $#{self.total}."
-    end
+       return "There is no discount to apply"
+     end
   end
 
     def void_last_transaction
